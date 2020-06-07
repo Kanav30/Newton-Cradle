134d6e0d091682a0dfce9971296477f3f8fe679a
Kanav Mathur <kanavmathur30@gmail.com>
2020-06-07 20:47:57 +0530

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,8 +1,8 @@
 class Bob {
     constructor(x, y, radius) {
       var options = {
-        isStatic: false  
-        
+        isStatic: false,
+        restitution: 0.4
       }
       this.body = Bodies.circle(x, y, radius,options);
       this.radius = radius;
