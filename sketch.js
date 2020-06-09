42f3c8d0bae09025820b639d6c10a70204f555ab
Kanav Mathur <kanavmathur30@gmail.com>
2020-06-09 21:37:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,34 +6,39 @@ const Render = Matter.Render;
 const Constraint = Matter.Constraint;
 
 var roof;
-var bob1;
+var bob1,bob2,bob3,bob4,bob5;
 var rope1,rope2,rope3,rope4,rope5;
 
 function setup() {
-	createCanvas(800, 500);
+	createCanvas(1600,700);
 	engine = Engine.create();
 	world = engine.world;
 	Engine.run(engine);
   
-BobDiameter=40;	
-bob1=new Bob(400,290,65);
-bob2=new Bob(465,290,65);
-bob3=new Bob(530,290,65);
-bob4=new Bob(335,290,65);
-bob5=new Bob(270,290,65);
-bob3.debug=true;
-roof = new Roof(400,45,375,30);
+bobDiameter=40;	
 
-rope1 = new Rope(bob1.body, roof.body,-BobDiameter*2, 0);
-rope2 = new Rope(bob2.body, roof.body,-BobDiameter*1, 0);
-rope3 = new Rope(bob3.body, roof.body,BobDiameter*2, 0);
-rope4 = new Rope(bob4.body, roof.body,BobDiameter*1, 0);
-rope5 = new Rope(bob5.body, roof.body,0, 0);
+var startBobPositionX = width/2;
+var startBobPositionY=height/4+250;
+
+bob1=new Bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+bob2=new Bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+bob3=new Bob(startBobPositionX,startBobPositionY,bobDiameter);
+bob4=new Bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
+bob5=new Bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+	
+
+roof = new Roof(800,45,550,30);
+
+rope1=new Rope(bob1.body,roof.body,-bobDiameter*2, 0)
+rope2=new Rope(bob2.body,roof.body,-bobDiameter*1, 0)
+rope3=new Rope(bob3.body,roof.body,0, 0)
+rope4=new Rope(bob4.body,roof.body,bobDiameter*1, 0)
+rope5=new Rope(bob5.body,roof.body,bobDiameter*2, 0)
 }
 function draw() {
 	rectMode(CENTER);
 	
-	background(255);
+	background(225);
 	bob1.display();
 	bob2.display();
 	bob3.display();
@@ -46,7 +51,6 @@ function draw() {
  rope3.display();
  rope2.display();
  
- keyPressed();
  
 }
 
@@ -54,10 +58,21 @@ function draw() {
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
 
-	  Matter.Body.applyForce(bob3.body,bob3.body.position,{x:50,y:+45});
-
+	  Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50 ,y:-45});
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
